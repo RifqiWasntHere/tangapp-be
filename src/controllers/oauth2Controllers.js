818fef03c4f2766675421bfa406ae7cb51f5ee18
RifqiNotWibu <rifqifadhillahh@gmail.com>
2024-05-01 22:36:17 +0700

    added existing user validation -> oauth controller

--- src/controllers/oauth2Controllers.js
+++ src/controllers/oauth2Controllers.js
@@ -1,13 +1,25 @@
+const { getUserbyEmail } = require('../../models/userModel')
+
 exports.googleAuthCallback = async (req, res, next) => {
   try {
     const { user } = req
+
+    const existingUser = await getUserbyEmail(user.emails[0].value).catch(
+      (error) => {
+        console.error('Error fetching user by email', error)
+        return res
+          .status(500)
+          .send('Internal Server Error while fetching user by email')
+      }
+    )
+
     // User exists -> generate token and send it away
     // User !exists -> send user's credentials for registration purpose
     if (existingUser) {
-      const token = await addJwtUser({
+      const token = {
         userId: existingUser.id,
         email: existingUser.email,
-      })
+      }
 
       return res.json({ token })
     } else {
